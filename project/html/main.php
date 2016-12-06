6c22a1561917714f74fe60baf9dbb908a8f1cd9d
Jorge Vale <jorge-vale1@hotmail.com>
2016-12-06 12:51:51 +0000

    first commit

---
+++ project/html/main.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<?-- Cerulean #4484CE
+	 Aluminium #D9D9D9-->
+
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+  <link rel="stylesheet" type="text/css" href="../css/menu.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+</head>
+
+<body>
+	<?php include('menu.php') ?>
+	<div id="restcontainer">
+	</br>
+		<form id="rsearch" action="" method="post"> 
+			<input id="srestaurant" type="text"  placeholder="Search for a restaurant.. " >
+		</form>
+			
+		<div class="rest">
+			<img src="../images/background.jpg">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>		
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>
+		<div class="rest">
+			<img src="../images/background.jpg" width="300" height="200" style="border:1px solid;border-color: #4484CE;">
+			<br>Name: name</br>
+			Location: location</br>
+			Average Score: 0/5 </br>
+			<div class="score">
+			</div>
+		</div>		
+	</div>
+	<div class="sidebar">
+		<h1>Categorias</h1>
+		<ul class="toolbar">
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+	</div>
+	
+</body>
+
+</html>
