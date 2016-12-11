8ae31145b7ed054036fee259261f9db03719feac
PedroMiguelGomes <gomespedromiguel@hotmail.com>
2016-12-11 23:30:25 +0000

    Update add_restaurant_action.php

--- project/add_restaurant_action.php
+++ project/add_restaurant_action.php
@@ -8,7 +8,7 @@
 	<div id="content">
 <?php
 	if ($result == null){
-		$stmt = $db->prepare('INSERT INTO User (idRestaurant, name, location, price, category, open, end, description, idOwner) VALUES (?,?,?,?,?,?,?,?,?)');
+		$stmt = $db->prepare('INSERT INTO Restaurant (idRestaurant, name, location, price, category, open, end, description, idOwner) VALUES (?,?,?,?,?,?,?,?,?)');
 		$stmt->execute(array(null,$_POST['name'],$_POST['location'],$_POST['price'],$_POST['category'],$_POST['open'],$_POST['end'],$_POST['description'],$_POST['idOwner']));
 		echo '<p>The restaurant was created sucessfully!</p>';
 	}else{
