032f2d0c7423c38975b0198d23fd0ab10410e8aa
PedroMiguelGomes <gomespedromiguel@hotmail.com>
2016-12-12 21:31:43 +0000

    Update add_restaurant_action.php

--- project/add_restaurant_action.php
+++ project/add_restaurant_action.php
@@ -5,8 +5,8 @@
 	<div id="content">
 <?php
 
-		$stmt = $db->prepare('INSERT INTO Restaurant (idRestaurant, name, location, price, category, open, end, description, idOwner) VALUES (?,?,?,?,?,?,?,?,?)');
-		$stmt->execute(array(null,$_POST['name'],$_POST['location'],$_POST['price'],$_POST['category'],$_POST['open'],$_POST['end'],$_POST['description'],$_SESSION['idUser']));
+		$stmt = $db->prepare('INSERT INTO Restaurant (idRestaurant, name, location, price, category, open, end, description, idOwner) VALUES (NULL,?,?,?,?,?,?,?,?)');
+		$stmt->execute(array($_POST['name'],$_POST['location'],$_POST['price'],$_POST['category'],$_POST['open'],$_POST['end'],$_POST['description'],$_SESSION['idUser']));
 		echo '<p>The restaurant was created sucessfully!</p>';
 ?>
 	</div>
