6255c6ba060b45500ee558b63cc432fd0b75be99
PedroMiguelGomes <gomespedromiguel@hotmail.com>
2016-12-13 00:49:29 +0000

    Create add_comment.php

---
+++ project/add_comment.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+	include_once('database/connection.php');
+	include_once('database/restaurant.php');
+	include('templates/header.php');
+	include('templates/add_comment.php');
+	include('templates/footer.php');
+ ?>
