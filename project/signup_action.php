fb66e61c6e473db45250f0d7424b2072121ac5ea
PedroMiguelGomes <gomespedromiguel@hotmail.com>
2016-12-15 22:08:58 +0000

    Update signup_action.php

--- project/signup_action.php
+++ project/signup_action.php
@@ -8,9 +8,8 @@
 	<div id="content">
 <?php
 	if ($result == null){
-		$options = ['cost' => 12];
 		$stmt = $db->prepare('INSERT INTO User (idUser, name, pass, age,gender, photo,type) VALUES (?,?,?,?,?,?,?)');
-		$stmt->execute(array($_POST['username'],$_POST['name'],password_hash($_POST['pass'], PASSWORD_DEFAULT, $options),$_POST['age'], $_POST['gender'],$_POST['photo'],$_POST['type']));
+		$stmt->execute(array($_POST['username'],$_POST['name'],sha1($_POST['pass']),$_POST['age'], $_POST['gender'],$_POST['photo'],$_POST['type']));
 		echo '<p>The user was created sucessfully!</p>';
 	}else{
 		echo '<p>Could not sign up because the user already exists!</p>';
