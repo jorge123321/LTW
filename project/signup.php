e0bb2f82adb4eb3acc4798560108aaa34cd3ce08
TADias <tasdsilva@gmail.com>
2016-12-15 04:02:56 +0000

    css for signup updated

--- project/signup.php
+++ project/signup.php
@@ -5,4 +5,5 @@
 	include('templates/header.php');
 	include('templates/signup.php');
 	include('templates/footer.php');
- ?>
+?>
+
--- project/templates/signup.php
+++ project/templates/signup.php
@@ -1,30 +1,29 @@
 <div id="content">
-	<h1> Make your own Restaurant</h1>
-	
-	<div id="signupForm">
-		<form id="submit" action="signup_action.php" method="post">
-			Username:<br>
-			<input type="text" name="username" value="required"><br>
-			Name:<br>
-			<input type="text" name="name" value="required"><br>
-			Pass:<br>
-			<input type="password" name="pass" value="required"><br>
-			Age:<br>
-			<input type="number" name="age" value="25" min="18" max="90" step="1"><br>
-			Gender:<br>
-			<div>
-				<input type="radio" name="gender" value="male">Male
-				<input type="radio" name="gender" value="female">Female<br>
-			</div>
-			Photo:<br>
-			<input type="text" name="photo" value="required">
-			Type:<br>
-			<div>
-				<input type="radio" name="type" value="reviewer">Reviewer
-				<input type="radio" name="type" value="owner">Owner<br>
-			</div>
-			<button type="submit"> Register </button>
-			
-		</form>
-	</div>
+		<h1> Make an account.</h1>
+		<div id="signupForm">
+			<form id="submit" action="signup_action.php" method="post">
+				Username:
+				<input type="text" class="uinfo" name="username" required><br>
+				Name:
+				<input type="text" class="uinfo" name="name" required><br>
+				Pass:
+				<input type="password" class="uinfo" name="pass" required><br>
+				Age:
+				<input type="number" name="age" value="25" min="18" max="90" step="1" required><br>
+				<div class="checkbox">
+					Gender:<br>
+					<input type="radio" name="gender" value="male" checked="true">Male
+					<input type="radio" name="gender" value="female">Female<br>
+				</div>
+				<br>
+				<input type="text" name="photo" value="required" style="display:none">
+				<div class="checkbox">
+					Type:<br>
+					<input type="radio" name="type" value="reviewer" checked="true">Reviewer
+					<input type="radio" name="type" value="owner">Owner<br>
+				</div>
+				<br>
+				</div><br>
+				<button type="submit" class="form_button"> Register </button>
+			</form>
 </div>
