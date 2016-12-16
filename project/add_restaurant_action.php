156144d42820be085234cc060f1bde1e94487fd7
Jorge Vale <jorge-vale1@hotmail.com>
2016-12-16 10:55:05 +0000

    fix

--- project/add_restaurant_action.php
+++ project/add_restaurant_action.php
@@ -15,25 +15,28 @@ session_start();
 		addRestaurant($db,$_POST['name'],$_POST['location'],$_POST['price'],$_POST['category'],$_POST['open'],$_POST['close'],$_POST['description'],$_SESSION['idUser']);
 		echo '<p>The restaurant was created sucessfully!</p>';
 		}
+		
 		$result = getRestaurantID($db, $_POST['name']);
 
-		
+
 		$n_files = count($_FILES['image']['name']);
 		for($i=0; $i<$n_files; $i++) {
 					$totalSize += $_FILES['image']['size'][$i];
 		}
+		
 		if($totalSize < 25000000){
 		for($i=0; $i<$n_files; $i++){
 			$extension = pathinfo($_FILES['image']['name'][$i], PATHINFO_EXTENSION);
-			if($extension != "jpg" && $extension != "png" && $extension != "jpeg") {
+			if($extension != "jpg" && $extension != "png" && $extension != "jpeg") 
 					echo "Sorry, only JPG, JPEG, and PNGfiles are allowed."; 
-			$picturePath = 'images/' . $_POST['name'] . '_' . $i . '.' . $extension;
-			if (move_uploaded_file($_FILES['image']['tmp_name'][$i], $picturePath)){
-				$stmt = $db->prepare('INSERT INTO Photo (idPhoto, idRestaurant, idOwner, text) VALUES (NULL,?,?,?)');
-				$stmt->execute(array($result['idRestaurant'], $_SESSION['idUser'], $picturePath));
+			else {
+				$picturePath = 'images/' . $_POST['name'] . '_' . $i . '.' . $extension;
+				if (move_uploaded_file($_FILES['image']['tmp_name'][$i], $picturePath)){
+					$stmt = $db->prepare('INSERT INTO Photo (idPhoto, idRestaurant, idOwner, text) VALUES (NULL,?,?,?)');
+					$stmt->execute(array($result['idRestaurant'], $_SESSION['idUser'], $picturePath));
+				}
 			}
 			}
-		}
 		}
 ?>
 	</div>
