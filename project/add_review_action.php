fe6e8b8436ffdec54a7a2037b18b851ca82cacbe
Jorge Vale <jorge-vale1@hotmail.com>
2016-12-16 10:57:21 +0000

    fixed echo

--- project/add_review_action.php
+++ project/add_review_action.php
@@ -6,10 +6,6 @@ session_start();
 ?>
 	<div id="content">
 <?php
-		echo '<p>' . $_POST['id'] . '</p>';
-		echo '<p>' . $_POST['score'] . '</p>';
-		echo '<p>' . $_POST['comment'] . '</p>';
-		echo '<p>' . $_SESSION['idUser'] . '</p>';
 		
 		$stmt = $db->prepare('INSERT INTO RestaurantReview (idReview, idReviewer, idRestaurant, score, text) VALUES (NULL,?,?,?,?)');
 		$stmt->execute(array($_SESSION['idUser'], $_POST['id'], $_POST['score'], $_POST['comment']));
